57a79a805adee3d2a319da0ec16b2d8355a54848
Stan Low <a22301613@yahoo.com.tw>
2015-07-28 18:17:09 +0800

    Basic test done

--- html-pipeline-flickr.gemspec
+++ html-pipeline-flickr.gemspec
@@ -19,5 +19,6 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency "html-pipeline", "~> 2.0"
+  spec.add_dependency "rinku", ">= 1.7"
   spec.add_development_dependency "rake", "~> 10.0"
 end
--- spec/html/pipeline/flickr_filter_spec.rb
+++ spec/html/pipeline/flickr_filter_spec.rb
@@ -4,20 +4,30 @@ describe HTML::Pipeline::FlickrFilter do
   subject { HTML::Pipeline::FlickrFilter }
   let(:flickr_url) { "https://www.flickr.com/photos/99434203@N05/9379906996" }
 
+  def filter(content, context = {})
+    pipeline = HTML::Pipeline.new [
+      HTML::Pipeline::AutolinkFilter,
+      HTML::Pipeline::FlickrFilter
+    ], context
+
+    result = pipeline.call(content)
+    result[:output].to_s
+  end
+
   it 'has a version number' do
     expect(HTML::Pipeline::Flickr::VERSION).not_to be nil
   end
 
   context "Default context" do
     it "generates with default options" do
-      expect(subject.to_html(flickr_url)).to eq(
+      expect(filter(flickr_url)).to eq(
         %(<a href="https://www.flickr.com/photos/99434203@N05/9379906996" ><img src="https://farm4.staticflickr.com/3787/9379906996_3ccabd5aae_b.jpg" alt="Ebony&Alex-011-IMGP4840" title="Ebony&Alex-011-IMGP4840" /></a>)
       )
     end
   end
   context "Multiple source" do
     it "generates two content" do
-      expect(subject.to_html(flickr_url + " test " + flickr_url)).to eq(
+      expect(filter(flickr_url + " test " + flickr_url)).to eq(
         %(<a href="https://www.flickr.com/photos/99434203@N05/9379906996" ><img src="https://farm4.staticflickr.com/3787/9379906996_3ccabd5aae_b.jpg" alt="Ebony&Alex-011-IMGP4840" title="Ebony&Alex-011-IMGP4840" /></a> test <a href="https://www.flickr.com/photos/99434203@N05/9379906996" ><img src="https://farm4.staticflickr.com/3787/9379906996_3ccabd5aae_b.jpg" alt="Ebony&Alex-011-IMGP4840" title="Ebony&Alex-011-IMGP4840" /></a>)
       )
     end
@@ -25,7 +35,7 @@ describe HTML::Pipeline::FlickrFilter do
 
   context "With maxwidth & maxheight options" do
     it "gets different size of image" do
-      result = subject.to_html(
+      result = filter(
         flickr_url,
         flickr_maxwidth: 100,
         flickr_maxheight: 200
@@ -39,7 +49,7 @@ describe HTML::Pipeline::FlickrFilter do
 
   context "With link_attr option" do
     it "generates link with attributes" do
-      result = subject.to_html(flickr_url, flickr_link_attr: "target='_blank'")
+      result = filter(flickr_url, flickr_link_attr: "target='_blank'")
 
       expect(result).to eq(
         %(<a href="https://www.flickr.com/photos/99434203@N05/9379906996" target='_blank'><img src="https://farm4.staticflickr.com/3787/9379906996_3ccabd5aae_b.jpg" alt="Ebony&Alex-011-IMGP4840" title="Ebony&Alex-011-IMGP4840" /></a>)
